6a7e05c7bbd282a231bbafa3b62a6a334b83d4b3
Marcos001 <santosMsantos01@gmail.com>
2017-05-24 18:32:43 -0300

    limiar simples e com outsu

--- outsu.py
+++ outsu.py
@@ -34,7 +34,8 @@ def binarizando_com_limiar(path_img):
     plt.show()
 
 def binarizando_com_outsu(path_img):
-    '''Aqui vem na binarização de Otsu.
+    '''
+    Aqui vem na binarização de Otsu.
     Este algoritmo irá permitir-nos obter de forma
     rápida e automaticamente o valor limite
     correto para escolher entre dois modo de histograma,
@@ -53,8 +54,47 @@ def binarizando_com_outsu(path_img):
     plt.title('Otsu thresholding'), plt.xticks([]), plt.yticks([])
     plt.show()
 
+def get_valores_limiar(path_img):
+    '''
+    calculando limiar
+    :param path_img:
+    :return:
+    '''
+
+    img = cv2.imread(path_img, 0)
+    # blur = cv2.GaussianBlur(img,(5,5),0)
+
+    # find normalized_histogram, and its cumulative distribution functio
+    hist = cv2.calcHist([img], [0], None, [256], [0, 256])
+    hist_norm = hist.ravel() / hist.max()
+    Q = hist_norm.cumsum()
+    bins = np.arange(256)
+    fn_min = np.inf
+    thresh = -1
+    for i in range(1, 256):
+        p1, p2 = np.hsplit(hist_norm, [i])  # probabilities
+        q1, q2 = Q[i], Q[255] - Q[i]  # cum sum of classes
+        if q1 == 0:
+            q1 = 0.00000001
+        if q2 == 0:
+            q2 = 0.00000001
+        b1, b2 = np.hsplit(bins, [i])  # weights
+        # finding means and variances
+        m1, m2 = np.sum(p1 * b1) / q1, np.sum(p2 * b2) / q2
+        v1, v2 = np.sum(((b1 - m1) ** 2) * p1) / q1, np.sum(((b2 - m2) ** 2) * p2) / q2
+        # calculates the minimization function
+        fn = v1 * q1 + v2 * q2
+        #if fn & lt & :
+        #   fn_min = fn
+        thresh = i
+    # find otsu's threshold value with OpenCV function
+    ret, otsu = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    print( thresh, ' <> ', ret)
+
+
 if __name__ == '__main__':
     ''
     #vendo_histograma_imagem('/home/mrv/PycharmProjects/Segmentation/data/imagens/natureza/fiolha.jpg')
     #binarizando_com_limiar('/home/mrv/PycharmProjects/Segmentation/data/imagens/natureza/fiolha.jpg')
-    binarizando_com_outsu('/home/mrv/PycharmProjects/Segmentation/data/imagens/natureza/fiolha.jpg')
+    binarizando_com_outsu('/home/mrv/PycharmProjects/Segmentation/data/imagens/retina/1.png')
+    #get_valores_limiar('/home/mrv/PycharmProjects/Segmentation/data/imagens/natureza/fiolha.jpg')
