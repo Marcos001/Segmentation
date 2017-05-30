1587dec356aa3b384654de2639ad644f1f9f67e4
Marcos001 <santosMsantos01@gmail.com>
2017-05-30 20:50:58 -0300

    criação das listas

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+'''
+Script para contagem do tempo de execeção dos algoritmos de segmentação K-means e Otsu com imagens da retina
+'''
+
 import timeit
 import glob
 from outsu import binarizando_com_outsu
@@ -7,21 +11,42 @@ from kmeans import kmeans_cv2
 arquivo_kmeans = open('tempo_kmeans.txt', 'w')
 arquivo_otsu = open('tempo_otsu.txt', 'w')
 
-#recoher a lista de imagens
-lista = glob.glob('/home/nig/PycharmProjects/Segmentation/data/imagens/retina/400/cinza/*.png')
+# lista de imagens 400 x 400 cinza
+lista_400_zinza = glob.glob('/home/nig/PycharmProjects/Segmentation/data/imagens/retina/400/cinza/*.png')
 
-#calculando o tempo com kmeans em imagens zinzas
+# lista de imagens 400 x 400 rgb
+lista_400_rgb = glob.glob('/home/nig/PycharmProjects/Segmentation/data/imagens/retina/400/rgb/*.png')
+
+# lista de imagens 800 x 800 cinza
+lista_800_zinza = glob.glob('/home/nig/PycharmProjects/Segmentation/data/imagens/retina/800/cinza/*.png')
+
+# lista de imagens 800 x 800 rgb
+lista_800_rgb = glob.glob('/home/nig/PycharmProjects/Segmentation/data/imagens/retina/800/rgb/*.png')
+
+
+#calculando o tempo com kmeans em imagens 400 x 400 cinzas - roda 30 vzs
 for i in range(3):
-    tempo_k_means = timeit.timeit("kmeans_cv2({})".format("'"+lista[i]+"', '/home/nig/PycharmProjects/Segmentation/data/segmt/kmeans"+str(i)+".png'"), setup="from __main__ import kmeans_cv2", number=1)
-    vari = 'execução '+str(i)+' com imagem '+lista[i]+' = '+str(tempo_k_means)
+    tempo_k_means = timeit.timeit("kmeans_cv2({})".format("'"+lista_400_zinza[i]+"', '/home/nig/PycharmProjects/Segmentation/data/segmt/kmeans_400_cinza"+str(i)+".png'"), setup="from __main__ import kmeans_cv2", number=1)
+    vari = 'execução '+str(i)+' com imagem '+lista_400_zinza[i]+' = '+str(tempo_k_means)
     arquivo_kmeans.write(vari+'\n')
-arquivo_kmeans.close()
+
+#calculando o tempo com kmeans em imagens 400 x 400 rgb - roda 30 vzs
+for i in range(3):
+    tempo_k_means = timeit.timeit("kmeans_cv2({})".format("'"+lista_400_rgb[i]+"', '/home/nig/PycharmProjects/Segmentation/data/segmt/kmeans_400_rgb"+str(i)+".png'"), setup="from __main__ import kmeans_cv2", number=1)
+    vari = 'execução '+str(i)+' com imagem '+lista_400_zinza[i]+' = '+str(tempo_k_means)
+    arquivo_kmeans.write(vari+'\n')
+
+
+arquivo_kmeans.close() #fechar o arquivo apos testes com o k-means
+
+
+
 
 #calculando o tempo com outsu em imagens zinzas
 for i in range(3):
     tempo_otsu = timeit.timeit(
-        "binarizando_com_outsu({})".format("'"+lista[i]+"', '/home/nig/PycharmProjects/Segmentation/data/segmt/otsu"+str(i)+".png'"),setup="from __main__ import binarizando_com_outsu", number=1)
-    vari = 'execução '+str(i)+' com imagem '+lista[i]+' = '+str(tempo_otsu)
+        "binarizando_com_outsu({})".format("'"+lista_400_zinza[i]+"', '/home/nig/PycharmProjects/Segmentation/data/segmt/otsu"+str(i)+".png'"),setup="from __main__ import binarizando_com_outsu", number=1)
+    vari = 'execução '+str(i)+' com imagem '+lista_400_zinza[i]+' = '+str(tempo_otsu)
     arquivo_otsu.write(vari+'\n')
 arquivo_otsu.close()
 
