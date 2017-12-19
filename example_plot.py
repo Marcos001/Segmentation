2917f430d8f7f0c45337d6b5efca57a4f335578c
Marcos001 <santosMsantos01@gmail.com>
2017-12-19 08:21:21 -0300

    mensurado resultado final dos 3 algoritmos

--- example_plot.py
+++ example_plot.py
@@ -172,6 +172,48 @@ def plot_confusion_matrix(cm, classes,
         plt.tight_layout()
         plt.show()
 
+def barchart():
+    n_groups = 3
+
+    means_men = (85.96,63.40,72.42)
+    #std_men = (2, 3, 4, 1, 2)
+
+    means_women = (25, 32, 34)
+    #std_women = (3, 5, 2, 3, 3)
+
+    fig, ax = plt.subplots()
+
+    index = np.arange(n_groups)
+    print('index is ',index)
+    bar_width = 0.35
+
+    opacity = 0.4
+    error_config = {'ecolor': '0.3'}
+
+    rects1 = plt.bar(index, means_men, bar_width,
+                     alpha=opacity,
+                     color='b',
+                     #yerr=std_men,
+                     error_kw=error_config,
+                     label='Acurácia')
+
+    rects2 = plt.bar(index + bar_width, means_women, bar_width,
+                     alpha=opacity,
+                     color='r',
+                     #yerr=std_women,
+                     error_kw=error_config,
+                     label='Sobreposição')
+
+    plt.xlabel('Algoritmos')
+    plt.ylabel(' Scores ')
+    plt.title(' Avaliação da Segmentação ')
+    plt.xticks(index + bar_width / 2, ('Otsu', 'Kmeans', 'WaterShed'))
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
 
 
 # https://www.pyimagesearch.com/2014/11/03/display-matplotlib-rgb-image/
+
+barchart()
